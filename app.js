4e34452344344f2710901f318500141902a48389
Eduardo Pedroso <eduardo.pedroso@matera.com>
2017-01-03 11:42:39 -0200

    Adding current-account extract

--- app.js
+++ app.js
@@ -1,19 +1,80 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
     
 // Definir a route principal
 app.get('/', function(req, res) {
-  res.send('PUCC Ionic Course');
+  res.send('IBK Mocks');
 });
 
-
-// Definir um endpoint da API
+// Authentication (Login)
 app.post('/oauth/token', function(req, res, next) {
-  res.send('teste');
+	let response = {
+		"access_token": "dh2931hud1dh91hd921j",
+		"expires_in": "string",
+		"scope": "string",
+		"token_type": "string"
+  }
+  res.send(response);
+})
+
+// Extract
+app.get('/extract/current-account/agency/account', function(req, res, next) {
+	let response = {
+      "data": {
+        "actualBalance": {
+          "date": "2017-01-02",
+          "value": 0
+        },
+        "balance": {
+          "available": 0,
+          "blocked": 0,
+          "partial": 0,
+          "restricted": 0,
+          "securedAccountLimit": 0,
+          "totalCharge": 0
+        },
+        "entries": [
+          {
+            "date": "2017-01-02",
+            "description": "Casas Bahia",
+            "document": "string",
+            "type": "Debit",
+            "value": 100.00
+          },
+          {
+            "date": "2017-01-02",
+            "description": "Burguer King",
+            "document": "string",
+            "type": "Debit",
+            "value": 40.12
+          },
+          {
+            "date": "2017-01-01",
+            "description": "Eduardo Pedroso",
+            "document": "string",
+            "type": "Credit",
+            "value": 80.00
+          },
+          {
+            "date": "2017-01-01",
+            "description": "Saldo",
+            "document": "string",
+            "type": "Balance",
+            "value": 119.00
+          },
+        ],
+        "lastBalance": {
+          "date": "2017-01-02",
+          "value": 0
+        }
+      },
+      "error": {}
+    }
+  res.send(response);
 })
 
 // Aplicação disponível em http://127.0.0.1:3000/
