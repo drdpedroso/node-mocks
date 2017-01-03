3083326cff072aa59dd2c4b1a3f479f2aa2a39e1
Eduardo Pedroso <eduardo.pedroso@matera.com>
2017-01-03 13:31:23 -0200

    More cors lol

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors());
     
 // Definir a route principal
 app.get('/', function(req, res) {
