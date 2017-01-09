784290ba0fc870323443c881f00c5227060bed93
Eduardo Pedroso <eduardo.pedroso@matera.com>
2017-01-09 17:43:13 -0200

    Adding investments mock

--- app.js
+++ app.js
@@ -87,6 +87,84 @@ app.get('/extract/current-account/agency/account', function(req, res, next) {
 })
 
 // Balance
+app.get('/balance/investment/agency/account', function(req, res, next) {
+	let response = {
+		"data": {
+			"available": 4570,
+			"blocked": 0,
+			"partial": 0,
+			"restricted": 0,
+			"securedAccountLimit": 0,
+			"totalCharge": 0
+		},
+		"error": {}
+	}
+  res.send(response);
+})
+
+app.get('/extract/investment/agency/account', function(req, res, next) {
+	let response = {
+      "data": {
+        "actualBalance": {
+          "date": "2017-01-02",
+          "value": 0
+        },
+        "balance": {
+          "available": 0,
+          "blocked": 0,
+          "partial": 0,
+          "restricted": 0,
+          "securedAccountLimit": 0,
+          "totalCharge": 0
+        },
+        "entries": [
+          {
+            "date": "2017-01-02",
+            "description": "Casas Bahia Invest",
+            "document": "string",
+            "type": "Debit",
+            "value": 100.00
+          },
+          {
+            "date": "2017-01-02",
+            "description": "Burguer King Invest",
+            "document": "string",
+            "type": "Debit",
+            "value": 40.12
+          },
+					{
+            "date": "2017-01-02",
+            "description": "Country Wine Invest",
+            "document": "string",
+            "type": "Debit",
+            "value": 924.40
+          },
+          {
+            "date": "2017-01-01",
+            "description": "Eduardo Pedroso Invest",
+            "document": "string",
+            "type": "Credit",
+            "value": 80.00
+          },
+          {
+            "date": "2017-01-01",
+            "description": "Saldo Invest",
+            "document": "string",
+            "type": "Balance",
+            "value": 119.00
+          },
+        ],
+        "lastBalance": {
+          "date": "2017-01-02",
+          "value": 0
+        }
+      },
+      "error": {}
+    }
+  res.send(response);
+})
+
+// Balance
 app.get('/balance/current-account/agency/account', function(req, res, next) {
 	let response = {
 		"data": {
